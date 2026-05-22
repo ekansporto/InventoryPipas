ea17e1872809cf6710ae0cdc7eab5063ff2958a5
Eka Nurfitria Septiani <ekanurfitriaseptiani@gmail.com>
2026-05-22 13:37:53 +0700

    pake wrangler

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     tsConfigPaths(),
     tailwindcss(),
-    tanstackStart({ target: "vercel" }),
+    tanstackStart({ target: "cloudflare-pages" }),
     viteReact(),
   ],
   resolve: {
